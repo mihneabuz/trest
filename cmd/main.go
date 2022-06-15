d09c7bd0691455f34a998f3bb6fbd06517ec3391
mihneabuz <mihneabuzatu88@gmail.com>
2022-06-15 20:18:53 +0300

    refactor and format

--- cmd/main.go
+++ cmd/main.go
@@ -1,65 +1,9 @@
 package main
 
 import (
-    "fmt"
-    "bufio"
-    "os"
-    "strings"
-    "trest/internal"
+	"trest/internal"
 )
 
-type command struct {
-    name string
-    exec func([]string)
-}
-
-func getCommands() []command {
-    commands := []command {
-        {
-            name: "post",
-            exec: func (args []string) {
-                fmt.Printf("POST: [%s] [type: %s] [body: %s]\n", args[0], args[1], args[2])
-                internal.Post(args)
-            },
-        },
-        {
-            name: "get",
-            exec: func (args []string) {
-                fmt.Printf("GET : [%s]\n", args[0])
-                res := internal.Get(args)
-                fmt.Println(res)
-            },
-        },
-        {
-            name: "exit",
-            exec:  func (_ []string) { os.Exit(0) },
-        },
-    }
-
-    return commands
-}
-
-func main()  {
-    commands := getCommands()
-
-    prompt := func() { fmt.Print(">>> ") }
-    scanner := bufio.NewScanner(os.Stdin)
-    prompt()
-    for scanner.Scan() {
-        s := strings.Split(scanner.Text(), " ")
-
-        for _, cmd := range commands {
-            if s[0] == cmd.name {
-                cmd.exec(s[1:])
-                break
-            }
-        }
-
-        prompt()
-    }
-
-    if err := scanner.Err(); err != nil {
-        fmt.Fprintln(os.Stderr, err)
-        os.Exit(1)
-    }
+func main() {
+	internal.StartCli()
 }
--- internal/request.go
+++ internal/request.go
@@ -9,32 +9,38 @@ import (
 )
 
 func parseResponse(resp *http.Response) string {
-    var body string
-    bodyBytes, err := ioutil.ReadAll(resp.Body)
-    if err != nil { body = err.Error() }
-    body = strings.Replace(string(bodyBytes), "\n", "\n> ", -1)
+	var body string
+	bodyBytes, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		body = err.Error()
+	}
+	body = strings.Replace(string(bodyBytes), "\n", "\n> ", -1)
 
-    content := resp.Header["Content-Type"]
+	content := resp.Header["Content-Type"]
 
-    res := fmt.Sprintf("=> [code: %s] [type: %s]\n> %s", resp.Status, content, body)
+	res := fmt.Sprintf("=> [code: %s] [type: %s]\n> %s", resp.Status, content, body)
 
-    return res
+	return res
 }
 
 func Post(args []string) string {
-    data := bytes.NewBuffer([]byte(args[1]))
+	data := bytes.NewBuffer([]byte(args[1]))
 
-    resp, err := http.Post(args[0], "application/json", data)
-    if err != nil { return err.Error() }
-    defer resp.Body.Close()
+	resp, err := http.Post(args[0], "application/json", data)
+	if err != nil {
+		return err.Error()
+	}
+	defer resp.Body.Close()
 
-    return parseResponse(resp)
+	return parseResponse(resp)
 }
 
 func Get(args []string) string {
-    resp, err := http.Get(args[0])
-    if err != nil { return err.Error() }
-    defer resp.Body.Close()
+	resp, err := http.Get(args[0])
+	if err != nil {
+		return err.Error()
+	}
+	defer resp.Body.Close()
 
-    return parseResponse(resp)
+	return parseResponse(resp)
 }
